cb5a5504f76b16ac665b3dc7ed089568d3f808c0
Michael Estok <mestok@nevermike.fios-router.home>
2018-12-11 15:22:19 -0500

    setup database

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+#import models
+from collection.models import Thing
+
+#set up automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+#register it
+admin.site.register(Thing, ThingAdmin)
