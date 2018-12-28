afd527bae3f22c1c5ede3534bb4194a37a8823af
Michael Estok <mestok@nevermike.fios-router.home>
2018-12-28 13:04:30 -0500

    registrations and login

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -13,8 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import (
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete,
+)
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from collection import views
 
@@ -28,5 +34,20 @@ urlpatterns = [
         name='contact'),
     path('things/<slug>/', views.thing_detail,
         name='thing_detail'),
+    path('things/<slug>/edit/',
+        views.edit_thing, name='edit_thing'),
+    path('accounts/password/reset', password_reset,
+        {'template_name': 'registration/password_reset_form.html'},
+        name="password_reset"),
+    path('accounts/password/reset/done', password_reset_done,
+        {'template_name': 'registration/password_reset_done.html'},
+        name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>', password_reset_confirm,
+        {'template_name': 'registration/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    path('accounts/password/done/', password_reset_complete,
+        {'template_name': 'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+    path('account/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
 ]
